908705f45b751cc70d29607a042054b6abb85415
wyj <wyj@wyjs-Mac-mini.local>
2016-01-27 19:25:54 +0800

    xiugai

---
+++ TestGit/TestGitTests/TestGitTests.m
@@ -0,0 +1,40 @@
+//
+//  TestGitTests.m
+//  TestGitTests
+//
+//  Created by wyj on 16/1/27.
+//  Copyright (c) 2016年 wyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TestGitTests : XCTestCase
+
+@end
+
+@implementation TestGitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
